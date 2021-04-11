d1f8fc0381b634930818312f7ab61477e133556e
Kamaldeep <ksahni687@gmail.com>
2021-04-11 17:48:38 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://kamal123:kamal123@cluster0.bgl6m.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'myreallylongandsecretjwtsecretkey',
+};
---
+++ graphql/resolvers/users.js
@@ -0,0 +1,104 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
+const { SECRET_KEY } = require('../../config');
+
+const User = require('../../models/User');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require('../../utils/validators');
+
+const genToken = user => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+};
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { valid, errors } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors!', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found!';
+        throw new UserInputError('User not found!', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        errors.general = 'Wrong Credentials!';
+        throw new UserInputError('Wrong Credentials!', { errors });
+      }
+
+      const token = genToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } }
+    ) {
+      // Validate User Data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      // Check if User Exists
+      const user = await User.findOne({ username });
+
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken',
+          },
+        });
+      }
+
+      // Hash password before saving to DB
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = genToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
